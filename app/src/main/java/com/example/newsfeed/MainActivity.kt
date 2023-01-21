cb612d2531505424d15e5cb5835dee1d2d5b3477
Vishwas <VishwasDubey150@gmail.com>
2023-01-21 16:00:45 +0530

    first commit

---
+++ app/src/main/java/com/example/newsfeed/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.newsfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var rv=findViewById<RecyclerView>(R.id.rv)
+        rv.layoutManager=LinearLayoutManager(this)
+    }
+}
---
+++ app/src/main/java/com/example/newsfeed/newslistAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.newsfeed
+
+class newslistAdapter {
+
+
+}
