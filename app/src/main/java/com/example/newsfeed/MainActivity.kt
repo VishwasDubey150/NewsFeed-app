62429567cf64157bdfd3a3fccfff3f5a11236d17
Vishwas <VishwasDubey150@gmail.com>
2023-01-21 19:12:16 +0530

    recycler view finally done

--- app/src/main/java/com/example/newsfeed/MainActivity.kt
+++ app/src/main/java/com/example/newsfeed/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.newsfeed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NewsItemClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var rv=findViewById<RecyclerView>(R.id.rv)
         rv.layoutManager=LinearLayoutManager(this)
+        val items=fetchData()
+        val adapter:newslistAdapter= newslistAdapter(items,this)
+        rv.adapter=adapter
+    }
+
+    private fun fetchData(): ArrayList<String> {
+        val list=ArrayList<String>()
+        for (i in 0 until 100)
+        {
+            list.add("Item $i")
+        }
+    return list
+    }
+
+    override fun onItemClicked(item:String)
+    {
+        Toast.makeText(this,"clicked item is $item",Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/newsfeed/newslistAdapter.kt
+++ app/src/main/java/com/example/newsfeed/newslistAdapter.kt
@@ -1,6 +1,40 @@
 package com.example.newsfeed
 
-class newslistAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class newslistAdapter(private val items:ArrayList<String>,private val listener:NewsItemClicked) :RecyclerView.Adapter<NewsViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+        val viewHolder=NewsViewHolder(view)
+        view.setOnClickListener {
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
 
 
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        var currentItem=items[position]
+        holder.name.text=currentItem
+    }
+
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
+
+
+class NewsViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+{
+    var name:TextView=itemView.findViewById(R.id.name)
+}
+
+interface NewsItemClicked{
+    fun onItemClicked(item:String)
 }
